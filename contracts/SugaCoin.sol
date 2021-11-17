362e8851b37d4c2ab1bdc2784acf00ef084c2f40
Alexander Sandoval <alexander.sf0828@gmail.com>
2021-11-17 14:10:34 -0500

    manage decimals on SugaCoin

--- contracts/SugaCoin.sol
+++ contracts/SugaCoin.sol
@@ -4,6 +4,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SugaCoin is ERC20 {
     constructor(uint256 initialSupply) ERC20("SugaCoin", "SUGA") {
-        _mint(msg.sender, initialSupply);
+        _mint(msg.sender, initialSupply * 10**uint256(decimals()));
     }
 }
